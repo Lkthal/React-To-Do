3e712f3463bc5b1b8ebbb46cdd0700398ece7891
LKThal <lkthal@Thal-s-Macbook-Pro.local>
2018-02-03 00:27:49 -0800

    update todo

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ class App extends Component {
     super(props);
     this.state = {
       todos: [
-        {description: "Walk the cat", isCompleted: true },
-        {description: "Throw the dishes away", isCompleted: false },
-        {description: "Buy new dishes", isCompleted: false }
+        {id: 1, description: "Walk the cat", isCompleted: true },
+        {id: 2, description: "Throw the dishes away", isCompleted: false },
+        {id: 3, description: "Buy new dishes", isCompleted: false }
       ],
       newTodoDescription: ''
+      
     };
   }
 
@@ -34,24 +35,24 @@ class App extends Component {
    this.setState({todos: todos });
  }
 
- deleteToDo (index) {
-   const todos = this.state.todos.filter();
-   const todo = todos[index];
-   this.setState({todos: todos });
-  }
+ deleteTodo(id) {
+  this.setState((prevState) => ({
+    items: prevState.items.filter(item => item.id !== id),
+  }))
+}
 
   render() {
     return (
       <div className="App">
         <ul>
           {this.state.todos.map( (todo, index) =>
-            <ToDo key={ index } description= {todo.description} isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+            <ToDo key={ index } description= {todo.description} isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } onClick={ () => this.deleteTodo } />
           )}
         </ul>
         <form onSubmit={ (e) => this.handleSubmit(e) }>
           <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
           <input type="submit" />
-          <button text="delete"/>
+          <button text="Delete"/>
         </form>
       </div>
     );
